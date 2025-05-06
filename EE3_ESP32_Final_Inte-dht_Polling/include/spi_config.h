1531a7ccabcc22f4636199d126e862736210cce8
LynnnPush <102379056+LynnnPush@users.noreply.github.com>
2025-05-06 15:23:22 +0200

    Delete EE3_ESP32_Final_Inte-dht_Polling directory

--- EE3_ESP32_Final_Inte-dht_Polling/include/spi_config.h
+++
@@ -1,8 +0,0 @@
-#ifndef SPI_CONFIG_H
-#define SPI_CONFIG_H
-
-// SPI Host definitions
-#define RFID_SPI_HOST    SPI2_HOST  // RFID uses SPI2
-#define NRF_SPI_HOST     SPI3_HOST  // NRF24L01 uses SPI1
-
-#endif // SPI_CONFIG_H
--- EE3_ESP32_Final_Inte-dht_Polling/include/wifi_webserver.h
+++
@@ -1,8 +0,0 @@
-#ifndef WIFI_WEBSERVER_H
-#define WIFI_WEBSERVER_H
-
-void wifi_init_softap(void);
-void start_webserver(void);
-void wifi_webserver_init(void);
-
-#endif // WIFI_WEBSERVER_H
